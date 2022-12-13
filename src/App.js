ef1a7caf3d6801def1100a35c6a4875ee0c3ec28
Suliman Khan <sulimank418@gmail.com>
2022-12-13 13:07:03 +0500

    TEst

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import Home from "./pages/home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      createBrowserRouter(
+      createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Home />}/>
+        <Route path="login" element={<Login />}/>
+        </Route>
+        )
+        );
     </div>
   );
 }
